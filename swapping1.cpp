26f7ba5333c397e3faf6e493c7f32939569e737e
suwasini <165628407+suwasini@users.noreply.github.com>
2024-07-11 18:12:33 +0530

    Add files via upload

---
+++ swapping1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=4,b=5,temp;
+   {
+    cout<<"Before swapping"<<endl;
+    cout<<"a ="<<a<<",b ="<<b<<endl;
+   }
+   temp=a;
+   a=b;
+   b=temp;
+   {
+    cout<<"/nAfter swapping"<<endl;
+    cout<<"a ="<<a<<",b ="<<b<<endl;
+   }
+}
+
